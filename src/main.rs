adcd4c95bb3952a47b718efcf398394dd7fd6c8b
ruyot <tabeeb700@gmail.com>
2026-09-07 22:00:33 -0400

    dont need another channel

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use signal::SignallingMessage;
-use tokio::sync::mpsc;
 mod server;
 mod room;
 mod signal;
@@ -9,12 +7,8 @@ const ADDR:&str = "127.0.0.1:8080"; // Server address
 
 #[tokio::main]
 async fn main() {
-    
-    // Need a local messaging queue between the peer and the engine thread that goes both ways
-    let (engine_tx, mut engine_rx) = mpsc::unbounded_channel::<SignallingMessage>(); // Engine send and receive handles
-    let (server_tx, mut server_rx) = mpsc::unbounded_channel::<SignallingMessage>(); // Server send and receive handles 
 
-    let run_application = server::start(ADDR, engine_tx, server_rx).await;
+    let run_application = server::start(ADDR).await;
 
     match run_application {
         Ok(_) => {
@@ -25,18 +19,4 @@ async fn main() {
         }
 
     }
-
-    /* 
-    let run_engine = tokio::spawn::(webrtc::webrtc_engine(server_tx, engine_rx));
-
-    match run_engine {
-        Ok(_) => {
-            println!("Engine thread started succesfully")
-        }
-        Err(_) => {
-            println!("Engine thread failed to start")
-        }
-    }
-    */
-    
 }
