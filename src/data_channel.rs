9ad7d2867e34479042d7abe1fa1717807d3a2b0c
ruyot <tabeeb700@gmail.com>
2026-09-17 23:36:38 -0400

    data channel wip

--- src/data_channel.rs
+++ src/data_channel.rs
@@ -1,5 +1,6 @@
 use std::{error::Error, sync::Arc};
 use webrtc::data_channel::{DataChannel, DataChannelEvent};
+use crate::signal::InputMessage;
 
 pub async fn data_channel_helper(handle : Arc<dyn DataChannel>) -> Result<(), Box<dyn Error + Send + Sync>>{
     
@@ -9,6 +10,7 @@ pub async fn data_channel_helper(handle : Arc<dyn DataChannel>) -> Result<(), Bo
         match event {
             DataChannelEvent::OnOpen => {
                 println!("Data channel opened!");
+                // Add wasm listeners for browser events
             }
             DataChannelEvent::OnClose => {
                 println!("Data channel closed");
@@ -19,3 +21,10 @@ pub async fn data_channel_helper(handle : Arc<dyn DataChannel>) -> Result<(), Bo
 
     Ok(())   
 }
+
+/*
+To manipulate anything in chrome, wasm has to make a call to javascript, and javascript makes the call to the browser
+writing manual js code for every dom method eg document.getelemenetbyid is too much
+the browser vendors publish formal machine readable definitions of every api that exists in browsers called webidl
+the web-sys crate in rust is entirely mechanically generated inside build.rs using wasm-bindgens webIDL frontend and the webidl interface defintions for web apis.
+*/
